9b890540eeb24971baf393f3329a5fe67dafe780
jjzmajic <uros.m.perisic@gmail.com>
2019-02-21 12:45:54 -0800

    Setup daemon using psutils.

--- iqutefy.py
+++ iqutefy.py
@@ -3,31 +3,43 @@
 import os
 import signal
 import sys
+from pathlib import Path
 
 import daemon
 import psutil
 from daemon.pidfile import PIDLockFile
 from inotify_simple import INotify, flags
 
-pidfile='/tmp/qutefy.pid'
 
 def cleanup():
     os.remove(pidfile)
+    sys.exit()
+
+
+pidfile = '/tmp/iqutefy.pid'
+
+# check if pidfile refers to dead process
+# if so, remove it
+if os.path.isfile(pidfile):
+    with open(pidfile) as pf:
+        pid = int(pf.readline)
+    if not psutil.pid_exists(pid):
+        os.remove(pidfile)
 
 with daemon.DaemonContext(
+        detach_process=False,
         pidfile=PIDLockFile(pidfile),
-        signal_map={signal.SIGTERM: cleanup}
-):
+        signal_map={signal.SIGTERM: cleanup}):
     inotify = INotify()
     watch_flags = flags.CREATE | flags.MODIFY
     wd = inotify.add_watch(sys.argv[1], watch_flags)
-
     process = psutil.Process()
-    with process.oneshot():
-        ## check if qutebrowser instance is still running
-        while process.parent().status() == psutil.STATUS_RUNNING:
-            for event in inotify.read():
-                os.system('qutebrowser :config-source')
 
-        ## kill after qutebrowser exits
-        cleanup()
+    # inotify iterator runs out immediately
+    while True:
+        for event in inotify.read():
+            os.system('qutebrowser :config-source')
+
+    # kill after qutebrowser exits
+    # not reached on SIGKILL
+    cleanup()
